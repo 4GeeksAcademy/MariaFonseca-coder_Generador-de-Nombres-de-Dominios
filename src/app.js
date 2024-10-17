6432bb61ae2884aa9aae9c2d4aedbadecc9d4eda
María José Fonseca Castro <mfonsecac052@gmail.com>
2024-10-17 03:03:01 +0000

    Avance de buenas practicas

--- src/app.js
+++ src/app.js
@@ -5,21 +5,22 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 window.onload = function() {
-  //write your code here
-  let pronoun = ["the", "our"];
-  let adj = ["great", "big"];
-  let noun = ["jogger", "racoon"];
-  let domain = [".com", ".net", ".us", ".io"];
-  let result = "";
+  let pronounOptions = ["the", "our"];
+  let adjetiveOptions = ["great", "big"];
+  let nounOptions = ["jogger", "racoon"];
+  let domainOptions = [".com", ".net", ".us", ".io"];
+  let uniqueResult = [];
 
-  for (let pron = 0; pron < pronoun.length; pron++) {
-    for (let adject = 0; adject < adj.length; adject++) {
-      for (let nouns = 0; nouns < noun.length; nouns++) {
-        for (let dom = 0; dom < domain.length; dom++) {
-          result += `${pronoun[pron]}${adj[adject]}${noun[nouns]}${domain[dom]}<br>`;
-        }
-      }
-    }
-  }
-  document.getElementById("Generador").innerHTML = result;
+  pronounOptions.forEach(chosenPronoun => {
+    adjetiveOptions.forEach(chosenAdjetive => {
+      nounOptions.forEach(chosenNoun => {
+        domainOptions.forEach(chosenDomain => {
+          uniqueResult.push(
+            `${chosenPronoun}${chosenAdjetive}${chosenNoun}${chosenDomain}`
+          );
+        });
+      });
+    });
+  });
+  document.getElementById("Generador").innerHTML = uniqueResult.join("<br>");
 };
